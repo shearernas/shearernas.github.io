ee64f43e3ff17cc6e50084390a073c6aeaad9d2a
Nate Shearer <127697024+shearernas@users.noreply.github.com>
2025-05-09 16:20:47 -0600

    More dark mode work

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -6,17 +6,17 @@ export const ThemeToggle = () => {
 
     const [isDarkMode, setIsDarkMode] = useState(false);
 
-    {/*useEffect( () => {
+    useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
 
         if (storedTheme === "dark") {
-            setIsDarkMode(true);
+            setIsDarkMode(true)
             document.documentElement.classList.add("dark");
         } else {
-            localStorage.setItem("theme", "light");
+            localStorage.setItem("theme", "light")
             setIsDarkMode(false);
         }
-    }, []);*/}
+    }, []);
 
     const toggleTheme = () => {
         if (isDarkMode) {

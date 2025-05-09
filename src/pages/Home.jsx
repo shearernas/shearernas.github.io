95c379e26a8c42e046ac18add57c5b0deb825a81
shearernas <nates@shaw.ca>
2025-05-09 01:55:37 -0600

    Createds pages, set Route in App.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+export const Home = () => {
+
+return (
+    <div className="min-h-screen ">
+    {/*Theme Toggle*/}
+
+    {/*Background star effects*/}
+
+    {/*Navbar*/}
+    
+
+    {/*Main Content*/}
+
+
+    {/*Fototer*/}
+    </div>
+)
+};

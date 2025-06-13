86c066e6a485e19adf033aea67944b531dc7a5cf
Nate Shearer <127697024+shearernas@users.noreply.github.com>
2025-06-13 14:39:56 -0600

    Filling in content and spacing for About section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,4 @@
-import { Code, User } from "lucide-react";
+import { Code, CircleUserRound, Origami  } from "lucide-react";
 
 
 export const AboutSection = () => {
@@ -10,20 +10,24 @@ export const AboutSection = () => {
                 About <span className="text-primary"> Me</span>
             </h2>
 
-            <div className="flex flex-col items-center justify-center p-4">
+            <div className="flex flex-col items-center justify-center pt-5">
                 <img src="src/assets/images/portrait.png" alt="Nate Shearer portrait" className="w-48 h-48 md:w-64 md:h-64 rounded-2xl object-cover shadow-lg" />
             </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center text-center pt-14">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-start text-center pt-18">
                 <div className="space-y-6">
-                    <h3 className="text-2xl font-semibold">Placeholder text</h3>
+                    <h3 className="text-2xl font-semibold pb-6 mt-6">A multi-discipline designer who considers all approaches and perspectives...</h3>
 
-                    <p className="text-muted-foreground text-lg md:text-xl">
-                        Some more description placeholder text to fill out the about section. This is where I can talk about my background, skills, and what I bring to the table as a designer and developer.
+                    <p className="text-muted-foreground text-md md:text-lg pb-3">
+                        Placeholder text.
                     </p>
 
-                    <p className="text-muted-foreground text-lg md:text-xl">
-                        Some more description placeholder text to fill out the about section. This is where I can talk about my background, skills, and what I bring to the table as a designer and developer.
+                    <p className="text-muted-foreground text-md md:text-lg pb-3">
+                        To the right are some skills and beliefs that I specialize in and use to fully drive my personal design process, from initial research to final implementation. I chose to pursue these unique skills and beliefs during my time as an information design undergrad student and hope to continue building on them after my time as a student has concluded.
+                    </p>
+                        
+                    <p className="text-muted-foreground text-md md:text-lg pb-3">
+                        If we get the opportunity to connect, you will find that I also value kindness and mutual recognition as key personal values of mine that I try to bring to every interaction I have with others.
                     </p>
 
                     <div className="flex flex-col sm:flex-row gap-4 pt-4 justify-center">
@@ -37,31 +41,53 @@ export const AboutSection = () => {
                 </div>
 
 
-                <div className="grid grid-cols-1 gap-6">
+                <div className="grid grid-cols-1 gap-3">
                     <div className="gradient-border p-6 card-hover">
-                        <div className="flex items-start gap-4">
+                        <div className="flex items-start gap-5">
                             <div className="p-3 rounded-full bg-primary/10">
-                                <Code className="h-6 w-6 text-primary"/>
+                                <CircleUserRound className="h-6 w-6 text-primary"/>
                             </div>
                             <div className="text-left">
-                                <h4> React </h4>
-                                <p>
-                                    Showcase some of my skills in React.
+                                <h4 className="text-lg pb-4 text-primary font-medium"> User-centric design and research </h4>
+                                <p className="text-sm pb-2">
+                                    Knowledge on complicated technical topics doesn't always have to be locked behind a paywall or an intimidating, 400+ page manual.
+                                </p>
+                                <p className="text-sm">
+                                    I believe that technical learning tools should be open source and accessible, using information design principles to allow anyone to learn these often complex topics as they would learn to ride a bike.
                                 </p>
                             </div>
                         </div>
                     </div>
+
                     <div className="gradient-border p-6 card-hover">
-                    <div className="flex items-start gap-4">
+                        <div className="flex items-start gap-5">
                             <div className="p-3 rounded-full bg-primary/10">
-                                <User className="h-6 w-6 text-primary"/>
+                                <Origami className="h-6 w-6 text-primary"/>
+                            </div>
+                            <div className="text-left">
+                                <h4 className="text-lg pb-4 text-primary font-medium"> Design with purpose to promote real change </h4>
+                                <p className="text-sm pb-2">
+                                    Knowledge on complicated technical topics doesn't always have to be locked behind a paywall or an intimidating, 400+ page manual.
+                                </p>
+                                <p className="text-sm">
+                                    I believe that technical learning tools should be open source and accessible, using information design principles to allow anyone to learn these often complex topics as they would learn to ride a bike.
+                                </p>
                             </div>
                         </div>
                     </div>
-                    <div className="gradient-border p-6 card-hover">
-                    <div className="flex items-start gap-4">
+                    <div className="gradient-border p-6 card-hover w-fit">
+                    <div className="flex items-start gap-5">
                             <div className="p-3 rounded-full bg-primary/10">
                                 <Code className="h-6 w-6 text-primary"/>
+                            </div>
+                            <div className="text-left">
+                                <h4 className="text-lg pb-4 text-primary font-medium"> A unique, accessible approach to open technical knowledge </h4>
+                                <p className="text-sm pb-2">
+                                    Knowledge on complicated technical topics doesn't always have to be locked behind a paywall or an intimidating, 400+ page manual.
+                                </p>
+                                <p className="text-sm">
+                                    I believe that technical learning tools should be open source and accessible, using information design principles to allow anyone to learn these often complex topics as they would learn to ride a bike.
+                                </p>
                             </div>
                         </div>
                     </div>
